6d05d09124f89b41ab160d197f954846f2cf15ec
zzy-pku <2559449196@qq.com>
2025-05-16 22:10:57 +0800

    Update user.h

--- user.h
+++ user.h
@@ -2,9 +2,12 @@
 #define USER_H
 #include <QString>
 #include<qjsonobject.h>
+#include"itempost.h"
+#include"chatroom.h"
+#include<vector>
 class User {
 public:
-    User(const QString& username, const QString& hashedPw);
+    User(const QString& username, const QString& password);//创建
 
     // 序列化为JSON对象
     QJsonObject toJson() const;
@@ -14,8 +17,18 @@ public:
     QString getUsername() const;
     bool checkPassword(const QString& inputPw) const;
 
+    //行为方法
+    void releasePost(ItemPost* p);//发布商品
+    void deletePostr(ItemPost* p);//下架商品
+    void chat(chatroom* p);//加入聊天
+    void addFvourite(ItemPost* p);//添加收藏
 private:
     QString username;
-    QString hashedPassword; // SHA256哈希值
+    QString password;
+    std::vector<ItemPost*> postPointer;//指向发布的商品
+    std::vector<chatroom*> historyChat;//历史聊天记录
+    std::vector<ItemPost*> myFavourite;//收藏
+
+
 };
 #endif // USER_H
